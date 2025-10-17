0a2c83c4b544deb69563a1dd210b8267098d63cf
Rachel Ren <rachelren2025@gmail.com>
2025-10-17 01:20:37 -0400

    Automation Script

---
+++ experiment_automation.py
@@ -0,0 +1,84 @@
+# TESTING AUTOMATION SCRIPT FOR EXPERIMENTS
+import itertools, subprocess, csv, re
+
+########################################################################
+'''
+CHANGE THE FOLLOWING PARAMETERS AS NEEDED BETWEEN THESE LINES
+'''
+EXECUTABLE = "./gpu_project"
+GPU = "GPU_2" # CHANGE THE NAME FOR EACH GPU
+REPETITION = 2 # SMALL NUMBER FOR TESTING (to mitigate noise)
+
+# VERIFICATION THAT ALL WORKS
+problem_sizes = [1000, 5000]
+threads_per_block = [32, 64]
+num_blocks = [1, 2]
+ops = ["addition"]
+########################################################################
+
+# Updates the CSV FILE NAME WITH THE GPU NAME
+CSV_FILE = f"results_{GPU}.csv"
+
+# CSV FILE
+with open(CSV_FILE, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["GPU",
+                     "ProblemSize",
+                     "ThreadsPerBlock",
+                     "NumBlocks",
+                     "Operation",
+                     "Avg_SingleThreadTime(sec)",
+                     "Avg_KernelTime(sec)",
+                     "Avg_Speedup"])
+
+# COMBINATIONS AND RUNNING EXPERIMENTS
+for ps in problem_sizes:
+    for tpb in threads_per_block:
+        for nb in num_blocks:
+            for op in ops:
+                
+                # CAN DELETE THIS PRINT LATER (to see process) <------------------------
+                print(f"Experiment: {EXECUTABLE} {ps} {tpb} {nb} {op}")
+                
+                # LISTS TO COLLECT RESULTS
+                single_thread_times = [] # all single thread times
+                kernel_times = [] # all kernel times
+                speedups = [] # all speedups
+
+                # REPEAT FOR AVERAGING
+                for _ in range(REPETITION):
+                    # RUN THE CUDA PROGRAM FROM INSIDE PYTHON ON THE COMMAND LINE
+                    result = subprocess.run(f"{EXECUTABLE} {ps} {tpb} {nb} {op}",
+                                         shell=True, capture_output=True, text=True)
+                    out = result.stdout.strip()
+
+                    # READING CUDAs OUTPUT (REGEX)
+                    cpu_time_match = re.search(r"Single thread time\s*=\s*([0-9.]+)", out)
+                    gpu_time_match = re.search(r"Kernel time\s*=\s*([0-9.]+)", out)
+                    speedup_match = re.search(r"Speedup:\s*([0-9.]+)", out)
+
+                    if cpu_time_match and gpu_time_match and speedup_match:
+                        single_thread_times.append(float(cpu_time_match.group(1)))
+                        kernel_times.append(float(gpu_time_match.group(1)))
+                        speedups.append(float(speedup_match.group(1)))
+                    else:
+                        print("ERROR- OUTPUT COULD NOT BE PARSED:\n", out)
+
+                # SAVE AVERAGED RESULTS
+                if single_thread_times:
+                    avg_single = sum(single_thread_times) / len(single_thread_times)
+                    avg_kernel = sum(kernel_times) / len(kernel_times)
+                    avg_speed  = sum(speedups) / len(speedups)
+
+                    with open(CSV_FILE, "a", newline="") as f:
+                        csv.writer(f).writerow([GPU,
+                                                ps,
+                                                tpb,
+                                                nb,
+                                                op,
+                                                avg_single,
+                                                avg_kernel, avg_speed])
+                    # PRINT AVERAGED RESULTS (CAN DELETE LATER) <------------------------
+                    print(f"Average Speedup = {avg_speed:.2f}x\n")
+                else:
+                    print("No Valid Data\n")
